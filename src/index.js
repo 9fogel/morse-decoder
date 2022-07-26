c726a665aa950d906fa3817569f7c6ad051a8fd5
Vera Kirayonak <Vera_Kirayonak@epam.com>
2022-07-26 16:11:20 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -41,6 +41,46 @@ function decode(expr) {
     // write your solution here
 }
 
+function decode(expr) {
+    let morseStr = '';
+    
+    for (let i = 0; i < expr.length; i + 9) {
+      let letterCode = expr.slice(i, i + 10);
+      i += 10;
+  
+      if (letterCode === '**********') {
+        morseStr += ' ';
+        } else {
+          let letterMorse = '';
+        
+          for (let k = 0; k < letterCode.length; k + 1) {
+            let symbolMorse = letterCode.slice(k, k + 2);
+            k += 2;
+  
+            if (symbolMorse === '00') {
+              letterMorse += '';
+            } else if (symbolMorse === '10') {
+                letterMorse += '.';
+              } else if (symbolMorse === '11') {
+                  letterMorse += '-';
+                }
+          } 
+        
+
+        for (let key in MORSE_TABLE) {
+          if (key === letterMorse) {
+            
+            morseStr += MORSE_TABLE[key];
+          }
+        }
+        
+        } 
+      
+          
+    }
+    return morseStr;
+  }
+
 module.exports = {
     decode
 }
